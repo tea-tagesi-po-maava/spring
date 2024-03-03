b079cd9da3b226225bb596dd8052a1aadaecdf0a
srinath <g@gmail.com>
2024-03-03 15:05:37 +0530

    update

--- src/main/java/com/example/myapplication/MyApplication.java
+++ src/main/java/com/example/myapplication/MyApplication.java
@@ -2,14 +2,13 @@ package com.example.myapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
-@ComponentScan(basePackages = "com.example.myapplication") // Ensure that the packages from DemoApp are scanned
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.myapplication")
 public class MyApplication {
 
     public static void main(String[] args) {
