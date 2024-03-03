bdd761bbd182c3247277e12bd39a68c827536d8f
srinath <g@gmail.com>
2024-03-03 15:02:13 +0530

    files added

--- src/main/java/com/example/myapplication/MyApplication.java
+++ src/main/java/com/example/myapplication/MyApplication.java
@@ -3,37 +3,21 @@ package com.example.myapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-
-@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
-public class MyApplication {
-package com.mt.springmongo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Main Application controller.
- *
- * @author Srinath 
- */
 @Controller
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.mt.*")
-public class DemoApp {
+@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
+@ComponentScan(basePackages = "com.example.myapplication") // Ensure that the packages from DemoApp are scanned
+public class MyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApp.class, args);
+        SpringApplication.run(MyApplication.class, args);
     }
 
     @RequestMapping("/")
     public String index() {
         return "index.html";
-    }
-}
-    public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
     }
 }
