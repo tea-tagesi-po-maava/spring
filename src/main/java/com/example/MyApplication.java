50b7992383a1eb116f2ec07cf092aeb1cccec786
srinath <g@gmail.com>
2024-03-03 11:29:16 +0530

    update

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -1,3 +1,5 @@
+package com.example.MyApplication;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
